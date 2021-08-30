ca566a3cd2e49e39a04e04679d23900b4220c7ab
BuckwheatOne <33817412+buckwheatone@users.noreply.github.com>
2021-08-30 15:36:18 -0500

    update login, layout, register

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
-from forms import RegistrationForm
+from forms import RegistrationForm, LoginForm
 from werkzeug.security import check_password_hash, generate_password_hash
 
 # remove later
@@ -14,15 +14,14 @@ cards = [
 ]
 
 app = Flask(__name__)
-app.config.from_mapping(
-    SECRET_KEY='This is my secret key!')
+app.config['SECRET_KEY'] = 'This is my secret key!'
 
 @app.route("/")
 def index():
     return render_template("index.html") 
 
-@app.route("/signup", methods=["GET", "POST"]) 
-def signup():
+@app.route("/register", methods=["GET", "POST"]) 
+def register():
     form = RegistrationForm()
     if request.method == 'POST' and form.validate():
         username = form.username.data
@@ -35,29 +34,38 @@ def signup():
         #     generate_password_hash(request.form.get("password"))
         #     )
         return redirect("/login")
-    return render_template("signup.html", form=form) 
+    return render_template("register.html", form=form) 
 
-@app.route("/signin", methods=["GET", "POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     # need to validate: only username and password match
     
     # Forget any user_id
     # session.clear()
-    
+    form = LoginForm() 
     if request.method == 'POST': #and form.validate():
         # look up username and password in db
-
-            # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+        # Remember which user has logged in
+        # session["user_id"] = rows[0]["id"]
         return redirect("/dashboard") 
     
-    return render_template("signin.html") 
+    return render_template("login.html", form=form) 
 
 @app.route("/dashboard")
 def dashboard():
     return render_template("dashboard.html", cards=cards)
 
+@app.route("/forgotpassword")
+def forgot_password():
+    pass
+
+@app.route("/test") 
+def test():
+    form = LoginForm() 
+    return render_template("test.html", form=form)
+
 
 if __name__ == '__main__':
     # Note, per docs app.run is not good for production
     app.run(debug=True) 
+
--- forms.py
+++ forms.py
@@ -1,20 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, SubmitField 
+from wtforms import BooleanField, PasswordField, StringField, SubmitField 
 from wtforms.validators import DataRequired, Email, EqualTo, length
 
 class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
-    username = StringField('username', validators=[DataRequired(), length(min=3)])
-    password = PasswordField('Password', validators=[DataRequired(), length(
-                             min=8, 
-                             max=200, 
-                             message="Password must be at least 8 characters"
-                             ),
-                             EqualTo('confpass', message='Passwords must match')]) 
-    confpass = PasswordField('Confpass', validators=[DataRequired(), length(
-                             min=8, 
-                             max=200,
-                         )])
-    submit = SubmitField("Submit") 
+    username = StringField('Username', 
+                           validators=[DataRequired(), length(min=3, max=20)])
+    password = PasswordField('Password', validators=[DataRequired(), length(min=8, max=200)]) 
+    submit = SubmitField("Sign Up") 
 
     # add username and e-mail uniqueness validations here
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), length(min=8, max=200)]) 
+    remember = BooleanField("Remember me")
+    submit = SubmitField("Log in") 
