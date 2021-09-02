e59d32299f2f2220348c6a7a8837e6e04ec989fe
BuckwheatOne <33817412+buckwheatone@users.noreply.github.com>
2021-09-02 16:11:06 -0500

    add initial model

--- application.py
+++ application.py
@@ -1,5 +1,7 @@
+from datetime import datetime 
 from flask import Flask, flash, redirect, render_template, request, session, url_for
 from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -13,8 +15,36 @@ cards = [
     }
 ]
 
+
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'This is my secret key!'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True) 
+    username = db.Column(db.String(20), unique=True, nullable=False) 
+    email = db.Column(db.String(120), unique=True, nullable=False) 
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(), nullable=False) 
+    cards = db.relationship('Card', backref='author', lazy=True)
+
+    def __repr__(self): 
+        return f"User('{self.username}', '{self.email}', '{self.image_file}'')"
+
+class Card(db.Model): 
+    id = db.Column(db.Integer, primary_key=True) 
+    card_title = db.Column(db.String(100), nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    card_question = db.Column(db.Text, nullable=False) 
+    card_answer = db.Column(db.Text, nullable=False) 
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) 
+
+    def __repr__(self):
+        return f"Card('{self.card_title}'-- Q:'{self.card_question}' A:'{self.card_answer}')"
+    
 
 @app.route("/")
 def index():
