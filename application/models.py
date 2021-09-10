e1c091029b35e14b8d37fa5ab07865950cfa48e0
BuckwheatOne <33817412+buckwheatone@users.noreply.github.com>
2021-09-09 22:15:43 -0500

    update models

--- application/models.py
+++ application/models.py
@@ -9,10 +9,12 @@ class User(db.Model, UserMixin):
     image_file = db.Column(db.String(20), nullable=False, default='default.jpeg')
     password = db.Column(db.String(), nullable=False) 
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    active = db.Column(db.Integer, nullable=False, default=1)
+    # last login?
     cards = db.relationship('Card', backref='author', lazy=True)
 
     def __repr__(self): 
-        return f"User('{self.username}', '{self.email}', '{self.image_file}'')"
+        return f"User({self.username}, {self.email}, {self.image_file})"
 
 class Card(db.Model): 
     id = db.Column(db.Integer, primary_key=True) 
@@ -20,7 +22,19 @@ class Card(db.Model):
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     card_question = db.Column(db.Text, nullable=False) 
     card_answer = db.Column(db.Text, nullable=False) 
+    last_reviewed = db.Column(db.DateTime, nullable=False) 
+    next_review = db.Column(db.DateTime, nullable=False) 
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) 
 
     def __repr__(self):
-        return f"Card('{self.card_title}'-- Q:'{self.card_question}' A:'{self.card_answer}')"
+        return f"Card({self.card_title}-- Q:{self.card_question} A:{self.card_answer})"
+
+class Deck(db.Model):
+    id = db.Column(db.Integer, primary_key=True) 
+    name = db.Column(db.Text, nullable=False) 
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow) 
+    card_id = db.Column(db.Integer, db.ForeignKey('card.id'), nullable=False) 
+    card_count = db.Column(db.Integer, nullable=False)
+    
+    def __repr(self):
+        return f"Deck({self.name}, {self.card_count} Cards)"
